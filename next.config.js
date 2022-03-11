569a141dc85fc0e47a853bdc5995f4fc90cb520a
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-11 21:58:54 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ["api-assets.clashofclans.com"]
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true
     }
 };
 
