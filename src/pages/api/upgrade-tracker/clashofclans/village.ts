7d72fffb237744b830c91143d39914efc73db3f0
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-12 11:30:02 +0100

    removed village from the API, will be handled on client side

--- src/pages/api/upgrade-tracker/clashofclans/village.ts
+++ src/pages/api/upgrade-tracker/clashofclans/village.ts
@@ -7,7 +7,6 @@ import DatabaseManager from "@util/databaseManager";
 //@ts-ignore
 const Village: NextApiHandler = async (req, res) => {
     const playerTag = req.body.playerTag;
-    const village = req.body.village;
     const user: UserWithoutPassword = req.body.user;
     //Ensures the player's village is in the database
     await Util.getConnection();
@@ -15,15 +14,12 @@ const Village: NextApiHandler = async (req, res) => {
     if (!playerSchema) return res.redirect("/upgrade-tracker/clashofclans");
     //Ensures the village is from the session user
     if (!user.clashOfClansVillages?.includes(playerSchema.id)) return res.redirect("/upgrade-tracker/clashofclans");
-    //Ensures, if the village parameter is "builder", that the player unlocked the builder base
-    if (village && village == "builder" && !playerSchema.player.builderHallLevel) return res.redirect(`/upgrade-tracker/clashofclans/${playerTag.replace(/#/g, "")}/home`);
     const returnObj: {
         playerSchema: ClashOfClansVillage & { _id: any },
         village?: string
     } = {
         playerSchema: playerSchema
     };
-    if (village) returnObj.village = village;
     Util.ApiHandler.sendSuccess<{
         playerSchema: ClashOfClansVillage & { _id: any },
         village?: string
