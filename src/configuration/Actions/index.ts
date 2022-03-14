ddb0858abb0d6ba0a638d11ab133ca8794e17419
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-14 17:17:14 +0100

    added global loading slicer

--- src/configuration/Actions/index.ts
+++ src/configuration/Actions/index.ts
@@ -1,15 +1,18 @@
-import { errorSlicer } from "./error";
+import errorSlicer from "@actions/error";
 import { configureStore } from "@reduxjs/toolkit";
+import loadingSlice from "@actions/loading";
 
 export const store = configureStore({
     devTools: process.env.NODE_ENV == "development" ? true : false,
     reducer: {
-        error: errorSlicer.reducer
+        error: errorSlicer.reducer,
+        loading: loadingSlice.reducer
     }
 });
 
 export const actions = {
-    ...errorSlicer.actions
+    ...errorSlicer.actions,
+    ...loadingSlice.actions
 };
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
