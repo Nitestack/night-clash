ee068e32a5492e47baf6cb3bca62b79e3fbf1f94
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-14 20:34:11 +0100

    code formatted

--- src/components/Layout/LoadingScreen.tsx
+++ src/components/Layout/LoadingScreen.tsx
@@ -9,17 +9,15 @@ import styles from "@components/Layout/LoadingScreen.module.scss";
 const LoadingScreen: FC = () => {
     const numbers = [0, 1, 2, 3, 4, 5, 6, 7];
     return (
-        <>
-            <Center className="bg-lightmodebackground dark:bg-darkmodebackground min-h-[100vh]">
-                <div className={Util.classNames(styles.loader, "before:bg-lightmodebackground before:dark:bg-darkmodebackground after:bg-lightmodebackground after:dark:bg-darkmodebackground")}>
-                    {numbers.map((number) => (
-                        <div key={number} className={Util.classNames(styles.box, styles[`box${number}`])}><div/></div>
-                    ))}
-                    <div className={styles.ground}><div/></div>
-                </div>
-                <small className="block absolute bottom-45 text-2xl sm:text-5xl text-lightmodetext dark:text-darkmodetext text-center"> {Util.Constants.websiteApplicationName} </small>
-            </Center>
-        </>
+        <Center className="bg-lightmodebackground dark:bg-darkmodebackground min-h-[100vh]">
+            <div className={Util.classNames(styles.loader, "before:bg-lightmodebackground before:dark:bg-darkmodebackground after:bg-lightmodebackground after:dark:bg-darkmodebackground")}>
+                {numbers.map((number) => (
+                    <div key={number} className={Util.classNames(styles.box, styles[`box${number}`])}><div/></div>
+                ))}
+                <div className={styles.ground}><div/></div>
+            </div>
+            <small className="block absolute bottom-45 text-2xl sm:text-5xl text-lightmodetext dark:text-darkmodetext text-center"> {Util.Constants.websiteApplicationName} </small>
+        </Center>
     );
 };
 
