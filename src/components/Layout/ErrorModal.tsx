5b621f7034cb2656fcc23290c069ca1329a7d8f9
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-15 10:47:06 +0100

    edited import

--- src/components/Layout/ErrorModal.tsx
+++ src/components/Layout/ErrorModal.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 import { useEffect } from "react";
 import { connect, ConnectedProps } from "react-redux";
 import Modal from "@components/Modal";
-import type { RootState } from "src/configuration/Actions/index";
+import type { RootState } from "@util/types";
 import Util from "@util/index";
 
 const connector = connect((state: RootState) => {
