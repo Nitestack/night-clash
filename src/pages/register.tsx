58eb7cc453c1f3e5519ee0d961a7bf90c12329da
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-16 11:07:43 +0100

    rework of register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,43 +1,38 @@
 import type { NextPageWithConfiguration } from "@util/types";
-import type { ChangeEvent } from "react";
+import type { ChangeEvent, FormEvent, MouseEvent } from "react";
 import { useState } from "react";
 import Link from "@components/Elements/Link";
 import { LockClosedIcon } from "@heroicons/react/solid";
 import Center from "@components/Utilities/Center";
+import Input from "@components/Elements/Input";
+import Util from "@util/index";
 
 const RegisterPage: NextPageWithConfiguration = () => {
-    const [state, setState] = useState<{ password: string, errorMessage: string }>({ password: "", errorMessage: "" });
-    function onConfirm(ev: ChangeEvent<HTMLInputElement>) {
-        if (!ev.target.value || ev.target.value == "") return;
-        const confirmedPassword = ev.target.value;
-        if (!state.password || state.password == "") setState({ ...state, errorMessage: "Please enter a password first before confirming!" });
-        else if (state.password != confirmedPassword) setState({ ...state, errorMessage: "Password doesn't match!" });
-        else setState({ ...state, errorMessage: "" });
-    };
+    const [password, setPassword] = useState("");
+    const [confirmedPassword, setConfirmedPassword] = useState("");
     function savePassword() {
-        return (ev: ChangeEvent<HTMLInputElement>) => setState({ ...state, password: ev.target.value });
+        return (ev: ChangeEvent<HTMLInputElement>) => setPassword(ev.target.value);
     };
     function validatePassword() {
-        return (ev: ChangeEvent<HTMLInputElement>) => onConfirm(ev);
+        return (ev: ChangeEvent<HTMLInputElement>) => setConfirmedPassword(ev.target.value);
+    };
+    function onSubmit() {
+        return (ev: FormEvent<HTMLFormElement>) => {
+            if (password != confirmedPassword) {
+                ev.preventDefault();
+                Util.toast.error("Confirmed password does not match with password!");
+            };
+        };
     };
     return (
         <Center className="min-h-full p-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
-                <form className="mt-8 space-y-6" action="/api/auth/register" method="POST">
+                <form onSubmit={onSubmit()} className="mt-8 space-y-6" action="/api/user/register" method="POST">
                     <div className="rounded-md shadow-sm -space-y-px font-coc-description">
-                        <div>
-                            <input name="username" type="text" autoComplete="username" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-lg rounded-t-md" placeholder="Username"/>
-                        </div>
-                        <div>
-                            <input name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-lg" placeholder="E-Mail"></input>
-                        </div>
-                        <div>
-                            <input id="password" name="password" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-lg" placeholder="Password" onChange={savePassword()}/>
-                        </div>
-                        <div>
-                            <input id="confirmpassword" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-lg rounded-b-md" placeholder="Confirm Password" onChange={validatePassword()}></input>
-                            <p className="font-coc-description text-red-600 mt-2 text-lg text-center">{state.errorMessage}</p>
-                        </div>
+                        <Input name="username" type="text" autoComplete="username" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-lg rounded-t-md" placeholder="Username"/>
+                        <Input name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-lg" placeholder="E-Mail"/>
+                        <Input id="password" name="password" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-lg" placeholder="Password" onChange={savePassword()}/>
+                        <Input id="confirmpassword" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-lg rounded-b-md" placeholder="Confirm Password" onChange={validatePassword()}/>
                     </div>
                     <Center className="justify-between">
                         <div className="flex items-center">
