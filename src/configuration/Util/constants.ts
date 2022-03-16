c5ad7c78c5dbc6991a2d2934ce7f6b49f507ace8
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-16 11:07:32 +0100

    edited constants

--- src/configuration/Util/constants.ts
+++ src/configuration/Util/constants.ts
@@ -3,10 +3,12 @@ import ClashRoyaleConstants from "@constants/clashRoyale";
 
 
 export default class Constants {
-    public static websiteApplicationName = "NightClash";
-    public static websiteApplicationDescription = "is a community upgrade and stats tracker for a variety of Supercell Games!";
+    public static APPLICATION_NAME = "NightClash";
+    public static APPLICATION_DESCRIPTION = "is a community upgrade and stats tracker for a variety of Supercell Games!";
     public static CoC = ClashOfClansConstants;
     public static CR = ClashRoyaleConstants;
     public static ADMIN_ROLE_ID = "620932ba59e1667c0426444c";
     public static USER_ROLE_ID = "620932ba59e1667c0426444b";
+    public static MAXED_TAG_LENGTH = 10;
+    public static MIN_TAG_LENGTH = 7;
 };
