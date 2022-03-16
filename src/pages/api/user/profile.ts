56bdfedcf545c24b366e00088b6a54ea3be90026
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-16 18:38:18 +0100

    edited profile data for /account

--- src/pages/api/user/profile.ts
+++ src/pages/api/user/profile.ts
@@ -1,6 +1,7 @@
 import type { NextApiHandler } from "next";
 import Util from "@util/index";
 import DatabaseManager from "@util/databaseManager";
+import { ClashOfClansClanProfile, ClashOfClansPlayerProfile, ClashRoyalePlayerProfile, ClashRoyaleClanProfile, BrawlStarsClubProfile, BrawlStarsPlayerProfile } from "@models/user";
 
 Util.connectDB();
 
@@ -9,14 +10,41 @@ const UserProfile: NextApiHandler = async (req, res) => {
     try {
         const user = await DatabaseManager.User.findOne({ email: email });
         if (!user) return Util.ApiHandler.sendError(res, 0, { errorMessage: "User not found!" });
+        const villages: Array<ClashOfClansPlayerProfile> = [];
+        for (const clashOfClansVillage of user.clashOfClansVillages) {
+            const village = await DatabaseManager.getClashOfClansVillageById(clashOfClansVillage);
+            if (village) {
+                const pushObject: ClashOfClansPlayerProfile = {
+                    name: village.player.name,
+                    tag: village.playerTag,
+                    townHallLevel: village.player.townHallLevel,
+                    townHallWeaponLevel: village.player.townHallWeaponLevel
+                };
+                //@ts-ignore
+                if (village.player.clan) pushObject.clan = {
+                    name: village.player.clan.name,
+                    tag: village.player.clan.tag,
+                    iconUrl: village.player.clan.badgeUrls.large
+                };
+                villages.push(pushObject);
+            };
+        };
         Util.ApiHandler.sendSuccess<{
-            email: string,
-            name: string,
-            role: string
+            clashOfClansVillages: Array<ClashOfClansPlayerProfile>,
+            clashOfClansStatsTrackerPlayers: Array<ClashOfClansPlayerProfile>,
+            clashOfClansStatsTrackerClans: Array<ClashOfClansClanProfile>,
+            clashRoyaleStatsTrackerPlayers: Array<ClashRoyalePlayerProfile>,
+            clashRoyaleStatsTrackerClans: Array<ClashRoyaleClanProfile>,
+            brawlStarsStatsTrackerPlayers: Array<BrawlStarsPlayerProfile>,
+            brawlStarsStatsTrackerClans: Array<BrawlStarsClubProfile>
         }>(res, {
-            email: user.email,
-            name: user.name,
-            role: user.role
+            clashOfClansVillages: villages,
+            clashOfClansStatsTrackerPlayers: user.clashOfClansStatsTrackerPlayers,
+            clashOfClansStatsTrackerClans: user.clashOfClansStatsTrackerClans,
+            clashRoyaleStatsTrackerClans: user.clashRoyaleStatsTrackerClans,
+            clashRoyaleStatsTrackerPlayers: user.clashRoyaleStatsTrackerPlayers,
+            brawlStarsStatsTrackerClans: user.brawlStarsStatsTrackerClans,
+            brawlStarsStatsTrackerPlayers: user.brawlStarsStatsTrackerPlayers
         });
     } catch (err) {
         console.log(err);
