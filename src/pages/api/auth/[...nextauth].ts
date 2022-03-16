22d3c4dd51fde92e16671f556e4a7907ccd48df4
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-16 11:07:52 +0100

    rework of authentication system

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,12 @@
-import type { User } from "@models/user";
+//@ts-nocheck
 import Util from "@util/index";
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
-import omit from "omit";
+import clientPromise from "@util/mongoDB";
 
 export default NextAuth({
+    adapter: MongoDBAdapter(clientPromise),
     // Configure one or more authentication providers
     providers: [
         CredentialsProvider({
@@ -15,24 +17,24 @@ export default NextAuth({
             // e.g. domain, username, password, 2FA token, etc.
             // You can pass any HTML attribute to the <input> tag through the object.
             credentials: {
-                emailOrUsername: { label: "E-Mail or Username", type: "text", placeholder: "E-Mail or Username" },
+                email: { label: "E-Mail", type: "email", placeholder: "E-Mail" },
                 password: { label: "Password", type: "password", placeholder: "Password" }
             },
-            async authorize(credentials, req) {
+            async authorize(credentials) {
                 // You need to provide your own logic here that takes the credentials
                 // submitted and returns either a object representing a user or value
                 // that is false/null if the credentials are invalid.
                 // e.g. return { id: 1, name: "J Smith", email: "jsmith@example.com" }
                 // You can also use the `req` object to obtain additional parameters
                 // (i.e., the request IP address)
-                const response = await Util.Axios.post("/api/auth/authorize", credentials, {
-                    baseURL: process.env.NEXTAUTH_URL
+                const response = await Util.Axios.post("/api/user/authorize", {
+                    email: credentials?.email,
+                    password: credentials?.password
                 });
-                const user: User = response.data;
+                const user = response.data;
                 // If no error and we have user data, return it
-                if (response.status == 200 && user) {
-                    return user;
-                } else {
+                if (response.status == 200 && user) return user;
+                else {
                     // If you return null then an error will be displayed advising the user to check their details.
                     return null;
                     // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter        
@@ -42,7 +44,8 @@ export default NextAuth({
     ],
     pages: {
         signIn: "/login",
-        signOut: "/logout"
+        signOut: "/logout",
+        error: "/login"
     },
     secret: process.env.AUTH_SECRET,
     session: {
@@ -50,27 +53,6 @@ export default NextAuth({
     },
     jwt: {
         secret: process.env.JWT_SECRET
-    },
-    callbacks: {
-        async jwt({ token, account, profile, user, isNewUser }) {
-            let newToken = token;
-            // Persist the OAuth access_token to the token right after signin
-            if (user) {
-                newToken.customUser = omit("hash", user);
-            };
-            if (account) {
-                newToken.accessToken = account.access_token;
-            };
-            return newToken;
-        },
-        async session({ session, token, user }) {
-            // Send properties to the client, like an access_token from a provider.
-            session.accessToken = token.accessToken;
-            //Assigning the users information
-            //@ts-ignore
-            session.user = { ...omit("email", session.user), ...token.customUser };
-            return session;
-        }
     },
     debug: process.env.NODE_ENV == "development"
 });
