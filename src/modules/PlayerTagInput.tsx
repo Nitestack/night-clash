07521591c0d0cf21b32c0a19ef98db05bb9d040a
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-17 23:34:35 +0100

    forwardRef

--- src/modules/PlayerTagInput.tsx
+++ src/modules/PlayerTagInput.tsx
@@ -4,6 +4,7 @@ import Util from "@util/index";
 import Button from "@components/Elements/Button";
 import type { ButtonProps } from "@components/Elements/Button";
 import { HashtagIcon, SearchIcon } from "@heroicons/react/outline";
+import omit from "omit";
 
 const PlayerTagInput: FC<{
     element: string;
@@ -18,7 +19,7 @@ const PlayerTagInput: FC<{
                     <HashtagIcon className="w-6"/>
                 </Button>
             </div>
-            <Input placeholder={element == "clan" ? (club ? "Club" : Util.toCapitalize(element)) : Util.toCapitalize(element)} {...inputProps}/>
+            <Input {...omit("ref", inputProps)} type="text" minLength={Util.Constants.MIN_TAG_LENGTH} maxLength={Util.Constants.MAXED_TAG_LENGTH} placeholder={element == "clan" ? (club ? "Club" : Util.toCapitalize(element)) : Util.toCapitalize(element)}/>
             <div className="-ml-[1px] flex">
                 <Button {...searchButtonProps}>
                     <SearchIcon className="w-6"/>
