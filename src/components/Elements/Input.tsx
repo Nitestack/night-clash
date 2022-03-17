e2f1f63d06ca1e8e764c7281a6daf534439fc34b
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-17 23:29:11 +0100

    forwardRef

--- src/components/Elements/Input.tsx
+++ src/components/Elements/Input.tsx
@@ -1,9 +1,9 @@
 import Util from "@util/index";
 import type { DetailedHTMLProps, FC, InputHTMLAttributes } from "react";
+import { forwardRef } from "react";
 
-const Input: FC<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>> = (props) => {
-    return (
-        <input {...props} className={Util.classNames("font-coc-description text-xl appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-lg", props.className)}/>
-    );
-};
+const Input = forwardRef<HTMLInputElement, DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>>((props, ref) => (
+    <input {...props} ref={ref} className={Util.classNames("font-coc-description text-xl appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-lg", props.className)}/>
+));
+
 export default Input;
