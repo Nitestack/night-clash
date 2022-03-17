966bf217769fe424ee2e3f15606a5b185e9b2735
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-17 23:31:02 +0100

    added more properties to the omit function

--- src/modules/ClashOfClansTrophyCount.tsx
+++ src/modules/ClashOfClansTrophyCount.tsx
@@ -12,7 +12,7 @@ const ClashOfClansTrophyCount: FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElemen
     const { trophyCount, village, cursed } = props;
     const newVillage = village || "home";
     return (
-        <div {...omit(["trophyCount", "village", "trophyCountProps"], props)} className={Util.classNames("flex items-center whitespace-nowrap", props.className)}>
+        <div {...omit(["trophyCount", "village", "trophyCountProps", "imgClassName", "cursed"], props)} className={Util.classNames("flex items-center whitespace-nowrap", props.className)}>
             {!cursed ? <p {...props.trophyCountProps}>{trophyCount}</p> : undefined}
             <img className={Util.classNames("w-5", props.imgClassName)} src={`/Images/Clash of Clans/${Util.toCapitalize(newVillage)}/Trophies.png`}/>
             {cursed ? <p {...props.trophyCountProps}>{trophyCount}</p> : undefined}
