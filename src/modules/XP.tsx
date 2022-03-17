5403751489971ce28eba3c7e2cc56918e6ca2b26
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-17 23:28:28 +0100

    added max and min length to the input

--- src/modules/XP.tsx
+++ src/modules/XP.tsx
@@ -1,13 +1,14 @@
 import type { DetailedHTMLProps, FC, HTMLAttributes } from "react";
 import Util from "@util/index";
 import styles from "@modules/XP.module.scss";
+import omit from "omit";
 
 const XP: FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
     cr?: boolean,
     expLevel: number
 }> = (props) => {
     return (
-        <div {...props} className={Util.classNames(props.className, styles.xp, props.cr ? styles[props.expLevel >= Util.Constants.CR.maxLevel ? "cr-max" : "cr"] : styles.coc)}>{props.expLevel}</div>
+        <div {...omit(["expLevel", "cr", "className"], props)} className={Util.classNames(props.className, styles.xp, props.cr ? styles[props.expLevel >= Util.Constants.CR.maxLevel ? "cr-max" : "cr"] : styles.coc)}>{props.expLevel}</div>
     );
 };
 export default XP;
