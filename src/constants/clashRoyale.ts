b4489275deaf945e3a0fb46fc57b00b1fac07c53
HydraNhani <hydra.qntp1764@gmail.com>
2022-03-30 18:03:11 +0200

    Clash Royale Miner Update

--- src/constants/clashRoyale.ts
+++ src/constants/clashRoyale.ts
@@ -21,11 +21,11 @@ class ClashRoyaleElixir {
     private static onePointFiveElixirCards = ["Mirror"];
     private static twoElixirCards =  ["Goblins", "Bomber", "Spear Goblins", "Ice Golem", "Bats", "Wall Breakers", "Rage", "Zap", "The Log", "Barbarian Barrel", "Giant Snowball"];
     private static threeElixirCards = ["Knight", "Archers", "Minions", "Skeleton Army", "Ice Wizard", "Guards", "Princess", "Miner", "Mega Minion", "Dart Goblin", "Goblin Gang", "Bandit", "Royal Ghost", "Skeleton Barrel", "Fisherman", "Firecracker", "Elixir Golem", "Cannon", "Tombstone", "Arrows", "Goblin Barrel", "Tornado", "Clone", "Earthquake", "Royal Delivery"];
-    private static fourElixirCards = ["Valkyrie", "Musketeer", "Baby Dragon", "Mini P.E.K.K.A", "Hog Rider", "Dark Prince", "Lumberjack", "Battle Ram", "Inferno Dragon", "Hunter", "Night Witch", "Mother Witch", "Zappies", "Flying Machine", "Magic Archer", "Battle Healer", "Skeleton Dragons", "Mortar", "Bomb Tower", "Tesla", "Furnace", "Goblin Cage", "Goblin Drill", "Fireball", "Freeze", "Poison", "Skeleton King", "Golden Knight"];
+    private static fourElixirCards = ["Valkyrie", "Musketeer", "Baby Dragon", "Mini P.E.K.K.A", "Hog Rider", "Dark Prince", "Lumberjack", "Battle Ram", "Inferno Dragon", "Hunter", "Night Witch", "Mother Witch", "Zappies", "Flying Machine", "Magic Archer", "Battle Healer", "Skeleton Dragons", "Mortar", "Bomb Tower", "Tesla", "Furnace", "Goblin Cage", "Goblin Drill", "Fireball", "Freeze", "Poison", "Skeleton King", "Golden Knight", "Mighty Miner"];
     private static fiveElixirCards = ["Giant", "Balloon", "Witch", "Barbarians", "Prince", "Wizard", "Minion Horde", "Bowler", "Executioner", "Ram Rider", "Rascals", "Cannon Cart", "Royal Hogs", "Electro Dragon", "Goblin Hut", "Inferno Tower", "Graveyard", "Giant Skeleton", "Royal Giant", "Archer Queen"];
     private static sixElixirCards = ["Sparky", "Elite Barbarians", "Goblin Giant", "Elixir Collector", "X-Bow", "Rocket", "Lightning"];
-    private static sevenElixirCards = ["P.E.K.K.A", "Lava Hound", "Royal Recruits", "Mega Knight", "Barbarian Hut"];
-    private static eightElixirCards = ["Golem", "Electro Giant"];
+    private static sevenElixirCards = ["P.E.K.K.A", "Lava Hound", "Royal Recruits", "Mega Knight", "Barbarian Hut", "Electro Giant"];
+    private static eightElixirCards = ["Golem"];
     private static nineElixirCards = ["Three Musketeers"];
 };
 
