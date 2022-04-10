9663bbf464406494bc3bc118c90c499a2b3baf05
HydraNhani <hydra.qntp1764@gmail.com>
2022-04-10 12:48:06 +0200

    edited url

--- src/configuration/Util/apolloClient.ts
+++ src/configuration/Util/apolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-    uri: "http://localhost:3000",
+    uri: process.env.NODE_ENV == "development" ? "http://localhost:3000" : "night-clash.vercel.app",
     cache: new InMemoryCache(),
 });
